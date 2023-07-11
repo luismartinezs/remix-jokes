28ed4f88d143b078110e6b343a2c766d1a6de25c
Luis Martinez <luismartinezwebdev@gmail.com>
2023-07-11 20:14:59 +0200

    feat: optimistic ui

--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -5,8 +5,10 @@ import {
   Link,
   isRouteErrorResponse,
   useActionData,
+  useNavigation,
   useRouteError,
 } from "@remix-run/react";
+import { JokeDisplay } from "~/components/joke";
 
 import { db } from "~/utils/db.server";
 import { badRequest } from "~/utils/request.server";
@@ -67,6 +69,26 @@ export const action = async ({ request }: ActionArgs) => {
 
 export default function NewJokeRoute() {
   const actionData = useActionData<typeof action>();
+  const navigation = useNavigation();
+
+  if (navigation.formData) {
+    const content = navigation.formData.get("content");
+    const name = navigation.formData.get("name");
+    if (
+      typeof content === "string" &&
+      typeof name === "string" &&
+      !validateJokeContent(content) &&
+      !validateJokeName(name)
+    ) {
+      return (
+        <JokeDisplay
+          canDelete={false}
+          isOwner={true}
+          joke={{ name, content }}
+        />
+      );
+    }
+  }
 
   return (
     <div>
