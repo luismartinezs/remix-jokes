d405f02fcb43bca172452f494537fb5302a7cee3
Luis Martinez <luismartinezwebdev@gmail.com>
2023-07-11 17:42:41 +0200

    feat: add new joek

--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -1,16 +1,50 @@
+import { json, type ActionArgs, redirect } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { db } from "~/utils/db.server";
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+  const name = formData.get("name");
+  const content = formData.get("content");
+
+  const errors = {
+    name: name ? null : "Name is required",
+    content: content ? null : "Content is required",
+  };
+
+  const hasErrors = Object.values(errors).some(Boolean);
+
+  if (hasErrors) {
+    return json(errors);
+  }
+
+  invariant(typeof name === "string", "Name should be a string");
+  invariant(typeof content === "string", "Content should be a string");
+
+  const joke = await db.joke.create({
+    data: { name, content },
+  });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
+  const errors = useActionData<typeof action>();
+
   return (
     <div>
       <p>Add your own hilarious joke</p>
-      <form method="post">
+      <Form method="post">
         <div>
           <label>
-            Name: <input type="text" name="name" />
+            Name: {errors?.name ?? <span>Name is required</span>}
+            <input type="text" name="name" />
           </label>
         </div>
         <div>
           <label>
-            Content: <textarea name="content" />
+            Content: {errors?.content ?? <span>Content is required</span>}
+            <textarea name="content" />
           </label>
         </div>
         <div>
@@ -18,7 +52,7 @@ export default function NewJokeRoute() {
             Add
           </button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
